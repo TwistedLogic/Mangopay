3c7ad6b04262c9a429f29e7c94775aff502cedf2
laurent <laurent.brieu@hotmail.fr>
2014-03-10 09:48:47 +0100

    New types of bank accounts + Fix for bank account details classes description + Hooks and Notifications

--- src/MangoPay/MangoPay.php
+++ src/MangoPay/MangoPay.php
@@ -9,108 +9,114 @@ require_once __DIR__ . '/common/common.inc';
  * as well as holds configuration/authorization data.
  */
 class MangoPay {
-    
+
     /////////////////////////////////////////////////
     // Config/authorization related props
     /////////////////////////////////////////////////
-
+    
     /**
      * Authorization token methods
      * @var \MangoPay\AuthorizationTokenManager
      */
     public $OAuthTokenManager;
-
+    
     /**
      * Configuration instance
      * @var \MangoPay\Configuration
      */
     public $Config;
-
+    
     /////////////////////////////////////////////////
     // API managers props
     /////////////////////////////////////////////////
-
+    
     /**
      * OAuth methods
      * @var ApiOAuth
      */
     public $AuthenticationManager;
-
+    
     /**
      * Clients methods
      * @var Client
      */
     public $Clients;
-
+    
     /**
      * Users methods
      * @var ApiUsers
      */
     public $Users;
-
+    
     /**
      * Wallets methods
      * @var ApiWallets
      */
     public $Wallets;
-
+    
     /**
      * Transfers methods
      * @var ApiTransfers
      */
     public $Transfers;
-
+    
     /**
      * Pay-in methods
      * @var ApiPayIns
      */
     public $PayIns;
-
+    
     /**
      * Pay-out methods
      * @var ApiPayOuts
      */
     public $PayOuts;
-
+    
     /**
      * Refund methods
      * @var ApiRefunds
      */
     public $Refunds;
-
+    
     /**
      * Card registration methods
      * @var ApiCardRegistrations
      */
     public $CardRegistrations;
-
+    
     /**
      * Pre-authorization methods
      * @var ApiCardPreAuthorization
      */
     public $CardPreAuthorizations;
-
+    
     /**
      * Card methods
      * @var ApiCards
      */
     public $Cards;
-
+    
     /**
      * Events methods
      * @var ApiEvents
      */
     public $Events;
-
+    
+    /**
+     * Hooks methods
+     * @var ApiHooks
+     */
+    public $Hooks;
+    
     /**
      * Constructor
      */
     function __construct() {
-
+    
         // default config setup
         $this->Config = new Configuration();
         $this->OAuthTokenManager = new AuthorizationTokenManager($this);
-
+    
         // API managers
         $this->AuthenticationManager = new ApiOAuth($this);
         $this->Clients = new ApiClients($this);
@@ -123,6 +129,7 @@ class MangoPay {
         $this->CardRegistrations = new ApiCardRegistrations($this);
         $this->Cards = new ApiCards($this);
         $this->Events = new ApiEvents($this);
+        $this->Hooks = new ApiHooks($this);
         $this->CardPreAuthorizations = new ApiCardPreAuthorizations($this);
     }
 }
